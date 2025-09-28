330e351d48c0b0ab5c76c3135c8062ecf46cba82
rohaanumair <m.rohaanumair@gmail.com>
2025-09-28 13:00:50 +0500

    tool name/desc overide

--- main.py
+++ main.py
@@ -3,24 +3,7 @@ from mcp.server.fastmcp import FastMCP
 mcp = FastMCP(name='hello-mcp', stateless_http=True)
 
 
-@mcp.tool()
-def search_online(query: str) -> str:
-    return f'Searching for {query}...'
-
-@mcp.tool()
-def get_weather(city: str) -> str:
-    return f'Weather in {city} is Sunny.'
-
-
-mcp_app = mcp.streamable_http_app()
-
-
-from mcp.server.fastmcp import FastMCP
-
-mcp = FastMCP(name='hello-mcp', stateless_http=True)
-
-
-@mcp.tool()
+@mcp.tool(name='Online Researher', description="This tool searches online and return result")
 def search_online(query: str) -> str:
     return f'Searching for {query}...'
 
